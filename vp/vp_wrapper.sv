15ff3079033f27622636f89f2d587f234bc60c6f
Yuhan Li <leonlee1999@outlook.com>
2021-11-26 11:49:26 -0500

    Updated vp_wrapper.sv

--- vp/vp_wrapper.sv
+++ vp/vp_wrapper.sv
@@ -4,11 +4,31 @@
 
 module vp_wrapper #(
     parameter P_ALGORITHM = "VTAGE",    // algorithm to be instantiated
-    parameter P_NUM_PRED = 2            // number of concurrent predictions
+    parameter P_NUM_PRED = 2,           // number of concurrent predictions
+    parameter P_CONF_WIDTH = 8,         // number of confidence counter bits
+    parameter P_GBH_LEN = 32
     // TODO: define more parameters for subblocks
 ) (
-    // TODO: define input and output signals ...
+    // TODO: add signals for VTAGE
+    input   logic                                           clk_i,          // main clock
+    input   logic                                           rst_i,          // active high reset
 
+    // forward input interface signals
+    input   logic [P_NUM_PRED-1:0][31:0]                    fw_pc_i,        // current instruction address
+    input   logic [P_NUM_PRED-1:0]                          fw_valid_i,     // current instruction address valid qualifier
+    input   logic [P_GBH_LEN-1:0]                           fw_gbh_i,       // input global branch history
+    // forward prediction interface signals
+    output  logic [P_NUM_PRED-1:0][31:0]                    pred_pc_o,      // forward input pc delay matched, used for update
+    output  logic [P_NUM_PRED-1:0][31:0]                    pred_result_o,  // prediction result
+    output  logic [P_NUM_PRED-1:0]                          pred_conf_o,    // prediction result's confidence, 1 if saturated, 0 else
+    output  logic [P_NUM_PRED-1:0]                          pred_valid_o,   // qualifies the prediction result
+
+    // validation interface (feedback) signals
+    input   logic [P_NUM_PRED-1:0][31:0]                    fb_pc_i,        // address of execution result feedback
+    input   logic [P_NUM_PRED-1:0][31:0]                    fb_actual_i,    // true execution result of the instruction
+    input   logic [P_NUM_PRED-1:0]                          fb_mispredict_i,// indicates misprediction
+    input   logic [P_NUM_PRED-1:0]                          fb_conf_i,      // indicates if the prediction confidence was saturated
+    input   logic [P_NUM_PRED-1:0]                          fb_valid_i      // valid qualifier of feedback interface
 );
 
 // define logic and signals here
@@ -36,11 +56,23 @@ module vp_wrapper #(
         else if(P_ALGORITHM == "BASELINE") begin
             baseline_top #(
                 // TODO: input parameters to vtage here...
+                .P_STORAGE_SIZE(P_STORAGE_SIZE),
+                .P_CONF_WIDTH(P_CONF_WIDTH),
+                .P_NUM_PRED(P_NUM_PRED)
             ) baseline_top (
                 // TODO: IO signals here
-                // .fw_pc_i(a),
-                // .fw_gbh_i(b),
+                .fw_pc_i(fw_pc_i),
+                .fw_valid_i(fw_valid_i),
+                .pred_pc_o(pred_pc_o),
+                .pred_result_o(pred_result_o),
                 // ...
+                .fw_pc_i(),
+                .fw_pc_i(),
+                .fw_pc_i(),
+                .fw_pc_i(),
+                .fw_pc_i(),
+                .fw_pc_i(),
+                .fw_pc_i()
             );
         end
 
