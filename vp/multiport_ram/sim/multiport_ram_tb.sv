e51df8ef1af339fa3fea54c9664f3fb8270464c1
Yuhan Li <50648549+BadSofa233@users.noreply.github.com>
2022-01-17 19:53:49 -0500

    Created IP source and testbench for multiport memory.

---
+++ vp/multiport_ram/sim/multiport_ram_tb.sv
@@ -0,0 +1,156 @@
+// This is the testbench for multiport memory.
+// It is able to handle 2 read requests and 2 write requests to all separate addresses by multipumping or banking.
+// Author: Yuhan Li
+// Jan 17, 2022
+
+`timescale 1ns / 1ps
+
+module multiport_ram_tb ();
+
+    localparam P_MEM_DEPTH = 16;
+    localparam P_MEM_WIDTH = 32;
+    localparam LP_INDEX_WIDTH = $clog2(P_MEM_DEPTH);
+
+    logic                                           clk;
+    logic                                           clk_mp;
+    
+    logic [LP_INDEX_WIDTH-1:0]                      rda_addr;       // read port A address
+    logic [LP_INDEX_WIDTH-1:0]                      rdb_addr;       // read port B address
+    
+    // read output interface signals
+    logic [P_MEM_WIDTH-1:0]                         rda_data_sim;   // read port A data
+    logic [P_MEM_WIDTH-1:0]                         rdb_data_sim;   // read port B data
+    logic [P_MEM_WIDTH-1:0]                         rda_data_dut;   // read port A data
+    logic [P_MEM_WIDTH-1:0]                         rdb_data_dut;   // read port B data
+
+    // write input interface signals
+    logic [LP_INDEX_WIDTH-1:0]                      wra_addr;       // write port A address
+    logic [P_MEM_WIDTH-1:0]                         wra_data;       // write port A data
+    logic                                           wra_valid;      // write port A enable
+    logic [LP_INDEX_WIDTH-1:0]                      wrb_addr;       // write port B address
+    logic [P_MEM_WIDTH-1:0]                         wrb_data;       // write port B data
+    logic                                           wrb_valid;      // write port B enable
+
+    // sim signals
+    logic [P_MEM_WIDTH-1:0]     mem[P_MEM_DEPTH-1:0];
+    logic [LP_INDEX_WIDTH-1:0]  raddr;
+    logic [LP_INDEX_WIDTH-1:0]  waddr;
+    logic [P_MEM_WIDTH-1:0]     wdata;
+    logic                       fwda2a;
+    logic                       fwdb2a;
+    logic                       fwda2b;
+    logic                       fwdb2b;
+    
+    // testbench signals
+    integer                     seed = $random();
+    logic                       match;
+
+    initial begin
+        clk = 0;
+        clk_mp = 1;
+        for(integer i = 0; i < P_MEM_DEPTH; i = i + 1) begin
+            mem[i] <= 0;
+        end
+        $display("Seed %d", seed);
+    end
+    
+    always #10 clk = ~clk;
+    always #5  clk_mp = ~clk_mp;
+    
+    // generate random addresses
+    always @(posedge clk) begin
+        raddr <= $urandom(seed+$time+1);
+        waddr <= $urandom(seed+$time+2);
+        wdata <= $urandom(seed+$time+3);
+    end
+    
+    always @(posedge clk) begin
+        rda_addr <= raddr;
+        rdb_addr <= ~raddr;
+    end
+    
+    // generate write drivers
+    always @(posedge clk) begin
+        wra_addr <= waddr;
+        wrb_addr <= ~waddr;
+        wra_data <= waddr;
+        wrb_data <= ~waddr;
+        wra_valid <= $urandom(seed+$time+4);
+        wrb_valid <= $urandom(seed+$time+5);
+    end
+    
+    // sim mem
+    
+    // foward enables
+    assign fwda2a = wra_valid && wra_addr == rda_addr;
+    assign fwdb2a = wrb_valid && wrb_addr == rda_addr;
+    assign fwda2b = wra_valid && wra_addr == rdb_addr;
+    assign fwdb2b = wrb_valid && wrb_addr == rdb_addr;
+
+    // port A
+    always @(posedge clk) begin
+        if(wra_valid) begin
+            mem[wra_addr] <= wra_data;
+        end
+        // forward write data
+        if(fwda2a) begin
+            rda_data_sim <= wra_data;
+        end
+        else if(fwdb2a) begin
+            rda_data_sim <= wrb_data;
+        end
+        else begin
+            rda_data_sim <= mem[rda_addr];
+        end
+    end
+    // port B
+    always @(posedge clk) begin
+        if(wrb_valid) begin
+            mem[wrb_addr] <= wrb_data;
+        end
+        // forward write data
+        if(fwda2b) begin
+            rdb_data_sim <= wra_data;
+        end
+        else if(fwdb2b) begin
+            rdb_data_sim <= wrb_data;
+        end
+        else begin
+            rdb_data_sim <= mem[rdb_addr];
+        end
+    end
+    
+    // dut
+    multiport_ram #(
+        .P_MEM_DEPTH        (P_MEM_DEPTH),
+        .P_MEM_WIDTH        (P_MEM_WIDTH),
+        .P_SIM              (0),
+        .P_METHOD           ("MULTIPUMPED")
+    ) dut (
+        .clk_i              (clk),
+        .clk_mp_i           (clk_mp),
+        .rda_addr_i         (rda_addr),
+        .rdb_addr_i         (rdb_addr),
+        .rda_data_o         (rda_data_dut),
+        .rdb_data_o         (rdb_data_dut),
+        .wra_addr_i         (wra_addr),
+        .wra_data_i         (wra_data),
+        .wra_valid_i        (wra_valid),
+        .wrb_addr_i         (wrb_addr),
+        .wrb_data_i         (wrb_data),
+        .wrb_valid_i        (wrb_valid)
+    );
+    
+    // compare
+    
+    assign match = (rda_data_dut == rda_data_sim) && (rdb_data_dut == rdb_data_sim);
+    initial begin
+        if(rda_data_dut != rda_data_sim) begin
+            $error("ERROR: rda_data_o mismatch, dut 0x%X sim 0x%X", rda_data_dut, rda_data_sim);
+        end
+        if(rdb_data_dut != rdb_data_sim) begin
+            $error("ERROR: rdb_data_o mismatch, dut 0x%X sim 0x%X", rda_data_dut, rda_data_sim);
+        end
+    end
+
+endmodule
---
+++ vp/multiport_ram/src/multiport_ram.sv
@@ -0,0 +1,112 @@
+// This is the IP for multiport memory.
+// It is able to handle 2 read requests and 2 write requests to all separate addresses by multipumping or banking.
+// In banked method, the memory will be devided into 4 banks
+// Author: Yuhan Li
+// Jan 17, 2022
+
+`ifndef P_MEM_DEPTH
+`define P_MEM_DEPTH 2048
+`endif
+
+`ifndef P_MEM_WIDTH
+`define P_MEM_WIDTH 32
+`endif
+
+`ifndef P_METHOD
+`define P_METHOD "MULTIPUMPED"
+`endif
+
+`ifndef P_NUM_BANK
+`define P_NUM_BANK 2048
+`endif
+
+module multiport_ram #(
+    parameter P_MEM_DEPTH         = `P_MEM_DEPTH,     // memory depth, accepted: any exponentials of 2
+    parameter P_MEM_WIDTH         = `P_MEM_WIDTH,     // memory width, accepted: 8, 16, or 32
+    parameter P_METHOD            = `P_METHOD,        // multiport method, accepted: "MULTIPUMPED" or "BANKED"
+    parameter P_NUM_BANK          = `P_NUM_BANK,      // number of banks, only used when P_METHOD == "BANKED", accepted: 4
+    localparam LP_INDEX_WIDTH     = $clog2(P_MEM_DEPTH)
+) (
+    // TB_GEN_DEF CLOCK clk_i
+    input   logic                                           clk_i,          // main clock
+    input   logic                                           clk_mp_i,       // multipumping clock
+    
+    // read input interface signals
+    // TB_GEN_DEF INTERFACE rda DIR I CTRL NONE
+    input   logic [LP_INDEX_WIDTH-1:0]                      rda_addr_i,     // read port A address
+    // TB_GEN_DEF INTERFACE rdb DIR I CTRL NONE
+    input   logic [LP_INDEX_WIDTH-1:0]                      rdb_addr_i,     // read port B address
+    
+    // read output interface signals
+    // TB_GEN_DEF INTERFACE rda DIR O CTRL NONE
+    output  logic [P_MEM_WIDTH-1:0]                         rda_data_o,     // read port A data
+    // TB_GEN_DEF INTERFACE rdb DIR O CTRL NONE
+    output  logic [P_MEM_WIDTH-1:0]                         rdb_data_o,     // read port B data
+
+    // write input interface signals
+    // TB_GEN_DEF INTERFACE wra DIR I CTRL VALID
+    input   logic [LP_INDEX_WIDTH-1:0]                      wra_addr_i,     // write port A address
+    input   logic [P_MEM_WIDTH-1:0]                         wra_data_i,     // write port A data
+    input   logic                                           wra_valid_i,    // write port A enable
+    // TB_GEN_DEF INTERFACE wrb DIR I CTRL VALID
+    input   logic [LP_INDEX_WIDTH-1:0]                      wrb_addr_i,     // write port B address
+    input   logic [P_MEM_WIDTH-1:0]                         wrb_data_i,     // write port B data
+    input   logic                                           wrb_valid_i     // write port B enable
+);
+
+    generate
+        if(P_METHOD == "MULTIPUMPED") begin
+            logic [P_MEM_WIDTH-1:0] mem[P_MEM_DEPTH-1:0];
+            initial begin
+                for(integer i = 0; i < P_MEM_DEPTH; i = i + 1) begin
+                    mem[i] <= 0;
+                end
+            end
+            // logic for multipumping
+            logic                       write_not_read_en;
+            logic                       write_not_read_sel;
+            logic [LP_INDEX_WIDTH-1:0]  addr_a;
+            logic [LP_INDEX_WIDTH-1:0]  addr_b;
+            logic [P_MEM_WIDTH-1:0]     rda_data_buf0;
+            logic [P_MEM_WIDTH-1:0]     rdb_data_buf0;
+            logic [P_MEM_WIDTH-1:0]     rda_data_buf1;
+            logic [P_MEM_WIDTH-1:0]     rdb_data_buf1;
+            
+            // always @(posedge clk_i) begin
+                // write_not_read <= ~write_not_read;
+            // end
+            
+            assign write_not_read_en = ~clk_i;
+            assign write_not_read_sel = clk_i;
+            assign addr_a = write_not_read_sel ? wra_addr_i : rda_addr_i;
+            assign addr_b = write_not_read_sel ? wrb_addr_i : rdb_addr_i;
+            
+            always @(posedge clk_mp_i) begin
+                if (wra_valid_i & write_not_read_en) begin
+                    mem[addr_a] <= wra_data_i;
+                end
+                rda_data_buf0 <= mem[addr_a];
+            end
+            always @(posedge clk_mp_i) begin
+                if (wrb_valid_i & write_not_read_en) begin
+                    mem[addr_b] <= wrb_data_i;
+                end
+                rdb_data_buf0 <= mem[addr_b];
+            end
+            
+            always @(posedge clk_mp_i) begin
+                if(write_not_read_en) begin
+                    rda_data_buf1 <= rda_data_buf0;
+                    rdb_data_buf1 <= rdb_data_buf0;
+                end
+            end
+            
+            assign rda_data_o = write_not_read_en ? rda_data_buf1 : rda_data_buf0;
+            assign rdb_data_o = write_not_read_en ? rdb_data_buf1 : rdb_data_buf0;
+            
+        end
+        else if(P_METHOD == "BANKED") begin
+        
+        end
+    endgenerate
+endmodule
