b434b9bf0b78b99ea44c46ccbbaa3b703d5f3a27
Yuhan Li <50648549+BadSofa233@users.noreply.github.com>
2022-02-13 15:55:24 -0500

    Added RVfpga sources and tests

---
+++ swerv/RVfpga/tests/AL_Operations/src/AL_Operations.S
@@ -0,0 +1,14 @@
+.globl main
+main:
+
+# Register t3 is also called register 28 (x28)
+li t3, 0x0					# t3 = 0
+
+REPEAT:
+	addi t3, t3, 6			# t3 = t3 + 6
+	addi t3, t3, -1		    # t3 = t3 - 1
+	andi t3, t3, 3			# t3 = t3 AND 3
+	beq  zero, zero, REPEAT	# Repeat the loop
+    nop
+
+.end
---
+++ swerv/RVfpga/tests/AL_Operations/test.tcl
@@ -0,0 +1,7 @@
+gtkwave::addSignalsFromList rvfpgasim.clk
+
+gtkwave::addSignalsFromList rvfpgasim.swervolf.swerv_eh1.swerv.ifu.ifu_i0_instr
+gtkwave::addSignalsFromList rvfpgasim.swervolf.swerv_eh1.swerv.ifu.ifu_i1_instr
+
+gtkwave::addSignalsFromList rvfpgasim.swervolf.swerv_eh1.swerv.dec.arf.gpr_banks(0).gpr(28).gprff.dout
+
