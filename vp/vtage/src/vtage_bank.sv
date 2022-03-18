05e64a5d620cc92507dda9fcbbc14a6c6cb996b4
Yuhan Li <50648549+BadSofa233@users.noreply.github.com>
2022-03-17 23:35:18 -0400

    vtage context table draft

--- vp/vtage/src/vtage_bank.sv
+++ vp/vtage/src/vtage_bank.sv
@@ -3,19 +3,121 @@
 // Author: Yuhan Li
 // Nov 14, 2021
 
+`default_nettype none
+
+`ifndef P_BANK
+`define P_BANK 0
+`endif
+
+`ifndef P_NUM_PRED
+`define P_NUM_PRED 2
+`endif
+
+`ifndef P_NUM_ENTRIES
+`define P_NUM_ENTRIES 256
+`endif
+
+`ifndef P_CONF_WIDTH
+`define P_CONF_WIDTH 8
+`endif
+
+`ifndef P_TAG_WIDTH
+`define P_TAG_WIDTH 8
+`endif
+
+`ifndef P_U_WIDTH
+`define P_U_WIDTH 2
+`endif
+
 module vtage_bank #(
-
+    parameter P_BANK = `P_BANK,
+    parameter P_NUM_PRED = `P_NUM_PRED,
+    parameter P_NUM_ENTRIES = `P_NUM_ENTRIES,
+    parameter P_CONF_WIDTH = `P_CONF_WIDTH,
+    parameter P_TAG_WIDTH = `P_TAG_WIDTH,
+    parameter P_U_WIDTH = `P_U_WIDTH,
+    localparam  LP_INDEX_WIDTH          = $clog2(P_NUM_ENTRIES)
 ) (
-
+    input logic clk_i,
+    input logic clk_ram_i,
+    input logic rst_i,
+    
+    input logic [P_NUM_PRED-1:0][LP_INDEX_WIDTH-1:0] fw_index_i,
+    input logic [P_NUM_PRED-1:0][P_TAG_WIDTH-1:0] fw_tag_i,
+    input logic [P_NUM_PRED-1:0] fw_valid_i,
+    
+    output logic [P_NUM_PRED-1:0][LP_INDEX_WIDTH-1:0] pred_result_o,
+    output logic [P_NUM_PRED-1:0][P_CONF_WIDTH:0] pred_conf_o,
+    output logic [P_NUM_PRED-1:0][P_TAG_WIDTH-1:0] pred_tag_o,
+    output logic [P_NUM_PRED-1:0][P_U_WIDTH-1:0] pred_useful_o,
+    output logic [P_NUM_PRED-1:0] pred_hit_o,
+    
+    input   logic [P_NUM_PRED-1:0][31:0]                fb_actual_i,        // true execution result of the instruction
+    output  logic [P_NUM_PRED-1:0][P_CONF_WIDTH:0]      fb_conf_i,          // original confidence fb
+    output  logic [P_NUM_PRED-1:0][LP_INDEX_WIDTH-1:0]  fb_index_i,         // original tag
+    output  logic [P_NUM_PRED-1:0][P_TAG_WIDTH-1:0]     fb_tag_i,           // original tag
+    output  logic [P_NUM_PRED-1:0][P_U_WIDTH-1:0]       fb_useful_i,        // original usefulness
+    input   logic [P_NUM_PRED-1:0]                      fb_mispredict_i,    // indicates misprediction
+    input   logic [P_NUM_PRED-1:0]                      fb_valid_i          // valid qualifier of feedback interface
 );
 
+    vtage_entry_t [P_NUM_PRED-1:0] vtage_fw_entry;
+    vtage_entry_t [P_NUM_PRED-1:0] vtage_fb_entry;
+    
+    // vtage table
+    multiport_ram #(
+        .P_MEM_DEPTH        (P_NUM_ENTRIES),
+        .P_MEM_WIDTH        ($bits(vtage_entry_t)),
+        .P_SIM              (`P_SIM),
+        .P_METHOD           ("MULTIPUMPED")
+    ) vtage_table (
+        .clk_i              (clk_i),
+        .clk_mp_i           (clk_ram_i),
+        
+        .rda_addr_i         (fw_index_i[0]),
+        .rda_data_o         (vtage_fw_entry[0]),
+        
+        .rdb_addr_i         (fw_index_i[1]),
+        .rdb_data_o         (vtage_fw_entry[1]),
+        
+        .wra_addr_i         (fb_index_i[0]),
+        .wra_data_i         (vtage_fb_entry[0]),
+        .wra_valid_i        (fb_wen[0]),
+        
+        .wrb_addr_i         (fb_index_i[1]),
+        .wrb_data_i         (vtage_fb_entry[1]),
+        .wrb_valid_i        (fb_wen[1])
+    );
+
     generate
-        for(genvar i = 0; i < P_NUM_ENTRIES; i = i + 1) begin : gen_vtage_entry
-            vtage_entry #(
-                
-            ) vtage_entry (
-            
-            );
+        if(P_BANK == 0) begin: gen_lvp_pred
+            for(int i = 0; i < P_NUM_PRED; i = i + 1) begin
+                assign pred_hit_o[i]        = 1'b1;
+                assign pred_result_o[i]     = vtage_fw_entry[i].value;
+                assign pred_conf_o[i]       = vtage_fw_entry[i].conf;
+                assign pred_tag_o[i]        = '0;
+                assign pred_useful_o[i]     = '0;
+            end
+        end
+        else begin: gen_vtage_pred
+            logic [P_NUM_PRED-1:0][P_TAG_WIDTH-1:0] fw_tag_d1;
+            always_ff @(posedge clk_i) begin
+                fw_tag_d1 <= fw_tag_i;
+            end
+            for(int i = 0; i < P_NUM_PRED; i = i + 1) begin
+                assign pred_hit_o[i]        = fw_tag_d1[i] == vtage_fw_entry[i].tag;
+                assign pred_result_o[i]     = vtage_fw_entry[i].value;
+                assign pred_conf_o[i]       = vtage_fw_entry[i].conf;
+                assign pred_tag_o[i]        = vtage_fw_entry[i].tag;
+                assign pred_useful_o[i]     = vtage_fw_entry[i].u;
+            end
+        end
+    endgenerate
+
+    generate
+        if(P_BANK == 0) begin: gen_lvp_fb
+        end
+        else begin: gen_vtage_fb
         end
     endgenerate
 
--- vp/vtage/src/vtage_entry.sv
+++ vp/vtage/src/vtage_entry.sv
@@ -2,29 +2,48 @@
 // Author: Yuhan Li
 // Nov 14, 2021
 
-module vtage_entry #(
-    parameter   P_NUM_PRED              = `P_NUM_PRED,
-    parameter   P_GBH_LENGTH            = `P_GBH_LENGTH,
-    parameter   P_NUM_ENTRIES           = `P_NUM_ENTRIES,
-    parameter   P_CONF_THRES_WIDTH      = `P_CONF_THRES_WIDTH,
-    parameter   P_HASH_LENGTH           = 15
-) (
-    input  logic [P_NUM_PRED-1:0]                               fw_get_i,
-    output logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fw_value_o,     // tristate value
-    output logic [P_NUM_PRED-1:0][P_CONF_THRES_WIDTH-1:0]       fw_conf_o,      // tristate confidence count
-    output logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fw_tag_o,       // tristate tag output
-    output logic [P_NUM_PRED-1:0][1:0]                          fw_useful_o,    // tristate usefulness output
-    output logic [P_NUM_PRED-1:0]                               fw_valid_o,     // 
+`ifndef P_CONF_WIDTH
+`define P_CONF_WIDTH 8
+`endif
+
+`ifndef P_TAG_WIDTH
+`define P_TAG_WIDTH 8
+`endif
+
+`ifndef P_U_WIDTH
+`define P_U_WIDTH 2
+`endif
+
+// module vtage_entry #(
+    // parameter   P_NUM_PRED              = `P_NUM_PRED,
+    // parameter   P_GBH_LENGTH            = `P_GBH_LENGTH,
+    // parameter   P_NUM_ENTRIES           = `P_NUM_ENTRIES,
+    // parameter   P_CONF_THRES_WIDTH      = `P_CONF_THRES_WIDTH,
+    // parameter   P_HASH_LENGTH           = 15
+// ) (
+    // input  logic [P_NUM_PRED-1:0]                               fw_get_i,
+    // output logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fw_value_o,     // tristate value
+    // output logic [P_NUM_PRED-1:0][P_CONF_THRES_WIDTH-1:0]       fw_conf_o,      // tristate confidence count
+    // output logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fw_tag_o,       // tristate tag output
+    // output logic [P_NUM_PRED-1:0][1:0]                          fw_useful_o,    // tristate usefulness output
+    // output logic [P_NUM_PRED-1:0]                               fw_valid_o,     // 
     
-    input  logic [P_NUM_PRED-1:0]                               fb_incr_conf_i, // increment confidence count
-    input  logic [P_NUM_PRED-1:0]                               fb_rst_conf_i,  // reset confidence
-    input  logic [P_NUM_PRED-1:0]                               fb_incr_use_i,  // increment usefulness
-    input  logic [P_NUM_PRED-1:0]                               fb_decr_use_i,  // reset usefulness, do we need this?
-    input  logic [P_NUM_PRED-1:0]                               fb_load_tag_i,  // load new tag
-    input  logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fb_tag_i,       // new tag to be loaded
-    input  logic [P_NUM_PRED-1:0]                               fb_load_value_i,// load new value
-    input  logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fb_value_i      // new value to be loaded
-);
+    // input  logic [P_NUM_PRED-1:0]                               fb_incr_conf_i, // increment confidence count
+    // input  logic [P_NUM_PRED-1:0]                               fb_rst_conf_i,  // reset confidence
+    // input  logic [P_NUM_PRED-1:0]                               fb_incr_use_i,  // increment usefulness
+    // input  logic [P_NUM_PRED-1:0]                               fb_decr_use_i,  // reset usefulness, do we need this?
+    // input  logic [P_NUM_PRED-1:0]                               fb_load_tag_i,  // load new tag
+    // input  logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fb_tag_i,       // new tag to be loaded
+    // input  logic [P_NUM_PRED-1:0]                               fb_load_value_i,// load new value
+    // input  logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fb_value_i      // new value to be loaded
+// );
 
 
-endmodule
+// endmodule
+
+typedef struct packed {
+    logic [P_CONF_WIDTH:0]      conf;
+    logic [P_TAG_WIDTH-1:0]     tag;
+    logic [P_U_WIDTH-1:0]       u;
+    logic [31:0]                value;
+} vtage_entry_t;
