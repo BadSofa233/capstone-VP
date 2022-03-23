cd4bc2d3dbd94038a48cb7bd431c0649d8accf91
Yuhan Li <50648549+BadSofa233@users.noreply.github.com>
2022-03-22 23:28:49 -0400

    vtage entry rework
    I HATE GIT!!!

--- vp/vtage/src/vtage_entry.sv
+++ vp/vtage/src/vtage_entry.sv
@@ -14,36 +14,101 @@
 `define P_U_WIDTH 2
 `endif
 
-// module vtage_entry #(
-    // parameter   P_NUM_PRED              = `P_NUM_PRED,
-    // parameter   P_GBH_LENGTH            = `P_GBH_LENGTH,
-    // parameter   P_NUM_ENTRIES           = `P_NUM_ENTRIES,
-    // parameter   P_CONF_THRES_WIDTH      = `P_CONF_THRES_WIDTH,
-    // parameter   P_HASH_LENGTH           = 15
-// ) (
-    // input  logic [P_NUM_PRED-1:0]                               fw_get_i,
-    // output logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fw_value_o,     // tristate value
-    // output logic [P_NUM_PRED-1:0][P_CONF_THRES_WIDTH-1:0]       fw_conf_o,      // tristate confidence count
-    // output logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fw_tag_o,       // tristate tag output
-    // output logic [P_NUM_PRED-1:0][1:0]                          fw_useful_o,    // tristate usefulness output
-    // output logic [P_NUM_PRED-1:0]                               fw_valid_o,     // 
+module vtage_entry #(
+    parameter   LP_INDEX_WIDTH          = 8,
+    parameter   P_CONF_WIDTH            = `P_CONF_WIDTH,
+    parameter   P_TAG_WIDTH             = `P_TAG_WIDTH,
+    parameter   P_U_WIDTH               = `P_U_WIDTH
+) (
+    input  logic                        clk_i,
+    input  logic                        rst_i,
     
-    // input  logic [P_NUM_PRED-1:0]                               fb_incr_conf_i, // increment confidence count
-    // input  logic [P_NUM_PRED-1:0]                               fb_rst_conf_i,  // reset confidence
-    // input  logic [P_NUM_PRED-1:0]                               fb_incr_use_i,  // increment usefulness
-    // input  logic [P_NUM_PRED-1:0]                               fb_decr_use_i,  // reset usefulness, do we need this?
-    // input  logic [P_NUM_PRED-1:0]                               fb_load_tag_i,  // load new tag
-    // input  logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fb_tag_i,       // new tag to be loaded
-    // input  logic [P_NUM_PRED-1:0]                               fb_load_value_i,// load new value
-    // input  logic [P_NUM_PRED-1:0][P_HASH_LENGTH-1:0]            fb_value_i      // new value to be loaded
-// );
+    // input  logic [P_TAG_WIDTH-1:0]      fw_tag_i,
+    // input  logic                        fw_valid_i,
+    
+    output logic [LP_INDEX_WIDTH-1:0]   pred_value_o,
+    output logic [P_CONF_WIDTH:0]       pred_conf_o,
+    output logic [P_TAG_WIDTH-1:0]      pred_tag_o,
+    output logic [P_U_WIDTH-1:0]        pred_useful_o,
+    
+    input  logic [P_TAG_WIDTH-1:0]      fb_tag_i,
+    output logic                        fb_tag_match_o,
+    output logic                        fb_alloc_avail_o,
+    
+    input  logic                        ud_incr_conf_i,
+    input  logic                        ud_rst_conf_i,
+    input  logic                        ud_incr_use_i,
+    input  logic                        ud_decr_use_i,
+    input  logic                        ud_rst_use_i,
+    input  logic                        ud_load_tag_i,
+    input  logic [P_TAG_WIDTH-1:0]      ud_tag_i,
+    input  logic                        ud_load_value_i,
+    input  logic [LP_INDEX_WIDTH-1:0]   ud_value_i
+);
 
+    logic [LP_INDEX_WIDTH-1:0]      value;
+    logic [P_CONF_WIDTH:0]          conf;
+    logic [P_TAG_WIDTH-1:0]         tag;
+    logic [P_U_WIDTH-1:0]           useful;
+    
+    initial begin
+        conf    = '0;
+        tag     = '0;
+        useful  = '0;
+    end
+    
+    // pred value output
+    assign pred_value_o     = value;
+    assign pred_conf_o      = conf;
+    assign pred_tag_o       = tag;
+    assign pred_useful_o    = useful;
+    
+    // feedback info
+    assign fb_tag_match_o   = fb_tag_i == tag;
+    assign fb_alloc_avail_o = useful == '0;
+    
+    // value update
+    always @(posedge clk_i) begin
+        if(ud_load_value_i) begin
+            value <= ud_value_i;
+        end
+    end
+    
+    // tag update
+    always @(posedge clk_i) begin
+        if(ud_load_tag_i) begin
+            tag <= ud_tag_i;
+        end
+    end
+    
+    // conf update
+    always @(posedge clk_i) begin
+        if(rst_i | ud_rst_conf_i) begin
+            conf <= '0;
+        end
+        else if(ud_incr_conf_i & ~conf[P_CONF_WIDTH]) begin // increment and saturate
+            conf <= conf + 1'b1;
+        end
+    end
+    
+    // useful update
+    always @(posedge clk_i) begin
+        if(rst_i | ud_rst_use_i) begin
+            useful <= '0;
+        end
+        else if(ud_incr_use_i & ~(&useful)) begin // increment and saturate to all 1s
+            useful <= useful + 1'b1;
+        end
+        else if(ud_decr_use_i & (|useful)) begin // decrement and saturate to 0
+            useful <= useful - 1'b1;
+        end
+    end
+    
+endmodule
 
-// endmodule
-
-typedef struct packed {
-    logic [P_CONF_WIDTH:0]      conf;
-    logic [P_TAG_WIDTH-1:0]     tag;
-    logic [P_U_WIDTH-1:0]       u;
-    logic [31:0]                value;
-} vtage_entry_t;
+// typedef struct packed {
+    // logic [P_CONF_WIDTH:0]      conf;
+    // logic [P_TAG_WIDTH-1:0]     tag;
+    // logic [P_U_WIDTH-1:0]       u;
+    // logic [31:0]                value;
+// } vtage_entry_t;
